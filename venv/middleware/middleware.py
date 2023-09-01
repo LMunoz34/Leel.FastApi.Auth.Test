a14a715cf69485026c4bc73c50320d9f57291268
LeelMoon <Leonel.Munoz@apchh.com>
2023-09-01 09:41:55 -0500

    added another buttonn to test loging in directly to apc auth and then sharing the session id to fastapi

--- venv/middleware/middleware.py
+++ venv/middleware/middleware.py
@@ -8,11 +8,12 @@ def add_middleware(app: FastAPI):
         "http://127.0.0.1",
         "http://127.0.0.1:5500",
         "http://127.0.0.1:8000",
+        "http://74.122.68.82",
     ]
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
